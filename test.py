9a90f3f8e2756a6dcc380ba8a6f3285320d6eba9
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2021-09-15 08:38:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ from WhirlWind import App
 
 app = App(host="localhost", port=80, secretKey="NoLeakOrIStealYourCookies")
 
+@app.path('/')
+def index():
+    return 'This is the index page of the website.'
