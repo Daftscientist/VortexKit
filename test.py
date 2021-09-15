67e93d6c24d46df9b657d7299db58d4c4f663712
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2021-09-15 08:41:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from WhirlWind import App
+## Imports ^
 
 app = App(host="localhost", port=80, secretKey="NoLeakOrIStealYourCookies")
+## Initial webserver setup ^
 
 @app.path('/')
 def index():
     return 'This is the index page of the website.'
+## Created a route, on the path `/`, that returns anything in the function return ^
