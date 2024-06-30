49bd9fb139f56c437af4f20fa00266465b8cb4a8
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-06-30 22:16:30 +0000

    rtd

---
+++ source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'WhirlWind'
+copyright = '2024, Leo Johnston'
+author = 'Leo Johnston'
+release = '0.1.5'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    # other extensions as needed
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../vortexkit'))  # Adjust the path as necessary
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. WhirlWind documentation master file, created by
+   sphinx-quickstart on Sun Jun 30 22:11:19 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to WhirlWind's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
